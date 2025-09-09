4422b5b4e27664df14f60a6678209e26f88d2fbb
( ᗜ ‸ ᗜ ) <boyu.cui1025@gmail.com>
2025-09-09 23:35:03 +1000

    error checks added

--- app.py
+++ app.py
@@ -17,20 +17,24 @@ def screenshot():
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.get("https://wplace.live/?lat=24.492667725032447&lng=118.17518521552732&zoom=14.282844653325695")
-
-    time.sleep(8)
+    driver.set_page_load_timeout(30)
 
     timestamp = datetime.now().strftime("%m-%d_%H-%M")
-    filename = os.path.join(CAPTURE_FOLDER, f"{timestamp}.png")
-    driver.save_screenshot(filename)
 
-    print(f"Captured: {filename}")
-    driver.quit()
+    try:
+        driver.get("https://wplace.live/?lat=24.492667725032447&lng=118.17518521552732&zoom=14.282844653325695")
+        time.sleep(8)
+        filename = os.path.join(CAPTURE_FOLDER, f"{timestamp}.png")
+        driver.save_screenshot(filename)
+        print(f"[{timestamp}]Captured: {filename}")
+    except Exception as e:
+        print(f"[{timestamp}]Capture failed {e}")
+    finally:
+        driver.quit()
 
-schedule.every().hour.do(screenshot)
+schedule.every(15).minutes.do(screenshot)
 
-print("Screenshot every hour...")
+print("Screenshot every 15 minutes...")
 screenshot()
 
 while True:
